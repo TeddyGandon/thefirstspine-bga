6c00e40a499c95626266aa732ae48ff8135c0600
TeddyGandon <teddy@coretizone.com>
2018-09-07 16:12:19 +0200

    Update Model.php

--- modules/api-wrapper/core/Model.php
+++ modules/api-wrapper/core/Model.php
@@ -14,7 +14,9 @@ class Model {
     protected function setOriginalAttributes($attributes)
     {
         $this->originalAttributes = json_decode(json_encode($attributes), true);
+        $this->originalAttributes = is_null($this->originalAttributes) ? array() : $this->originalAttributes;
         $this->currentAttributes = json_decode(json_encode($attributes), true);
+        $this->currentAttributes = is_null($this->currentAttributes) ? array() : $this->currentAttributes;
     }
 
     public function attributes()
@@ -24,24 +26,12 @@ class Model {
 
     public function __set($name, $value)
     {
-        if (!array_key_exists($name, $this->originalAttributes) && !empty($this->originalAttributes))
-        {
-            trigger_error("Property $name doesn't exists and cannot be set.", E_USER_ERROR);
-            return;
-        }
-
         $this->currentAttributes[$name] = $value;
     }
 
     public function __get($name)
     {
-        if (!array_key_exists($name, $this->originalAttributes) && !empty($this->originalAttributes))
-        {
-            trigger_error("Property $name doesn't exists and cannot be set.", E_USER_ERROR);
-            return null;
-        }
-
-        return $this->currentAttributes[$name];
+        return isset($this->currentAttributes[$name]) ? $this->currentAttributes[$name] : null;
     }
 
 }
